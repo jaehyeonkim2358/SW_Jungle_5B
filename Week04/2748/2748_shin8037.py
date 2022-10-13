ef2556e7d7c237111786a4aea6e1d3823fe8fc88
ChiWoo-Shin <shin8037@naver.com>
2022-10-13 18:27:23 +0900

    30840kb 76ms

---
+++ Week04/2748/2748_shin8037.py
@@ -0,0 +1,22 @@
+import sys
+
+input=sys.stdin.readline
+
+N=int(input())
+
+start=2
+dp=[0]*91
+dp[1]=1
+
+def solve(i):
+    global cnt
+    if N<=1:
+        print(dp[N])
+    elif dp[N]!=0:
+        print(dp[N])
+    else:
+        dp[i]=dp[i-2]+dp[i-1]        
+        solve(i+1)
+    
+
+solve(start)
