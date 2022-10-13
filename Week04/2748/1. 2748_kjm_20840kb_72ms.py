36a577fdcc9cf25cac2feee3bd973a8985f29354
unknown <markchck@gmail.com>
2022-10-13 22:08:04 +0900

    aa

--- Week04/2748/1. 2748_kjm_20840kb_72ms.py
+++ Week04/2748/1. 2748_kjm_20840kb_72ms.py
@@ -1,6 +1,9 @@
 # 메모리랑, 시간 적을 것
 # https://www.acmicpc.net/problem/2748
+
 # 재귀로 들어가면 시간 복잡도가 몇인지?? n^2인가?
+# https://blog.naver.com/ndb796/221233570962 
+# 이거보면 왜 n^2인지 그리고 왜 재귀가 구린 상황이 발생할 수 있는지 나옴.
 
 import sys
 input = sys.stdin.readline
--- 연습장.py
+++ 연습장.py
@@ -1,53 +1,17 @@
-import sys, heapq
-from collections import deque
+# Top-Down
+
+import sys
 input = sys.stdin.readline
-
+sys.setrecursionlimit(10**9)
 n = int(input())
-arr = []
-for i in range(n):
-    arr.append(list(map(int, list(input().rstrip()))))
 
-
-complete = [[0] * n for _ in range(n)]
-
-heap = []
-
-count_1 = 0
-
-def bfs(count, x, y):
-    heapq.heappush(heap, (count, x, y))
-    if complete[x][y] == 0:
-        complete[x][y] = 2
+count=0
+def recursion(n):
+    if n == 1:
+        count = 1
+    elif n ==2:
+        count = 2
     else:
-        complete[x][y] =1
-    con_x = [-1, 1, 0, 0]
-    con_y = [0, 0, -1, 1]
-
-    while heap:
-        count1, old_x, old_y = heapq.heappop(heap)
-
-        if old_x == n-1 and old_y == n-1:
-            return count1
-
-        for i in range(4):
-            new_x = old_x + con_x[i]
-            new_y = old_y + con_y[i]
-
-            if n > new_x >= 0 and n > new_y >= 0 and complete[new_x][new_y] == 0:
-
-                if arr[new_x][new_y] == 1:
-                    complete[new_x][new_y] = complete[old_x][old_y]
-                    heapq.heappush(heap, (count1+1, new_x, new_y))
-                else:
-                    heapq.heappush(heap, (count1-1, new_x, new_y))
-
-                if complete[new_x][new_y] == 0:
-                    complete[new_x][new_y] = 2
-                else:
-                    complete[new_x][new_y] = 1
-
-
-
-
-print(bfs(count_1, 0,0))
-print(complete)
+        count = recursion(n-1) + recursion(n-2)
+    return count
+print(recursion(n))
