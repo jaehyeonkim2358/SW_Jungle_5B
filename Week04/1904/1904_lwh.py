039d1cc5848722787bb5a4a8058da62abce567dd
wnhlee <2wheeh@gmail.com>
2022-10-14 17:47:56 +0900

    이원희 01타일 114328kb | 120ms | PyPy3

--- Week04/1904/1904_lwh.py
+++ Week04/1904/1904_lwh.py
@@ -2,19 +2,17 @@ import sys
 sys.stdin = open('Week04/1904/1904_lwh.txt', 'r')
 input = sys.stdin.readline
 
-tile = [0] * 1000001
+# tile = [0] * 1000001
 
 N = int(input())
-tile[1] = 1
-tile[2] = 2
+# tile[1] = 1
+# tile[2] = 2
+tile = [2, 1]
 
 def solve(n):
-    if n <= 2:
-        return tile[n]
-    
     for m in range(3, n+1):
-        tile[m] = (tile[m-1]%15746 + tile[m-2]%15746)%15746
+        tile[m%2] = (tile[0] % 15746 + tile[1] % 15746) % 15746
     
-    return tile[n] 
+    return tile[n%2] 
 
 print(solve(N))
