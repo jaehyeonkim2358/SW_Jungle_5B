12072265742ef6ca53214bb50e837ec414237dc1
Jaehyeon <jaehyeonkim2358@gmail.com>
2022-10-14 19:46:49 +0900

    김재현 평범한 배낭 33668kb 3704ms

--- Week04/12865/12865_jaehyeonkim2358_1.py
+++ Week04/12865/12865_jaehyeonkim2358_1.py
@@ -9,15 +9,12 @@ for _ in range(n):
     weight, value = map(int, sys.stdin.readline().split())
     if weight <= k:
         # 입력 받은 물건(무게, 가치)을 이용해서 비교한다.
-        for weight, value in knapsack:                  
-            # "구하려는 무게(k) ~ 현재 물건의 무게(weight)" 범위에서 
-            # 각각의 최대 가치합을 현재 물건의 가치(value)를 이용해서 갱신해준다.
-            for j in range(k, weight-1, -1):            
-                # 'j와 현재 물건의 무게(weight)차 만큼의 무게'가 가지고 있는 최대 가치합에
-                # 현재 물건의 가치(value)를 더해준 값과
-                # 기존의 무게j에서의 최대 가치합을 비교해준 뒤
-                # 둘 중 큰 값으로 갱신한다.
-                dp[j] = max(dp[j], value + dp[j - weight])
+        for j in range(k, weight-1, -1):            
+            # 'j와 현재 물건의 무게(weight)차 만큼의 무게'가 가지고 있는 최대 가치합에
+            # 현재 물건의 가치(value)를 더해준 값과
+            # 기존의 무게j에서의 최대 가치합을 비교해준 뒤
+            # 둘 중 큰 값으로 갱신한다.
+            dp[j] = max(dp[j], value + dp[j - weight])
 
 
 sys.stdout.write(f'{dp[k]}')
