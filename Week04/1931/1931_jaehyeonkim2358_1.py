54bccbcd87e6c36b8c45c15f750677ed5fd0f984
Jaehyeon <jaehyeonkim2358@gmail.com>
2022-10-16 22:15:35 +0900

    김재현 회의실 배정 52400kb 276ms

--- Week04/1931/1931_jaehyeonkim2358_1.py
+++ Week04/1931/1931_jaehyeonkim2358_1.py
@@ -1,16 +1,15 @@
 import sys
 
-# 시작시간을 기준으로 오름차순 정렬, 시작시간이 같으면 끝시간으로 오름차순 정렬
+# 끝시간을 기준으로 오름차순 정렬, 끝시간이 같으면 시작 시간으로 오름차순 정렬
 
 n = int(sys.stdin.readline().rstrip())
-time = sorted([tuple(map(int, sys.stdin.readline().split())) for _ in range(n)], key= lambda x: (x[0], x[1]))
+time = sorted([tuple(map(int, sys.stdin.readline().split())) for _ in range(n)], key= lambda x: (x[1], x[0]))
 
-# 회의실이 예약된 마지막시간부터 첫시간 순으로 탐색
-start = time[-1][1]
+end = 0
 count = 0
-for s, e in time[::-1]:
-    if start >= e:
-        start = s
+for s, e in time:
+    if end <= s:
+        end = e
         count += 1
 
 sys.stdout.write(f'{count}')
