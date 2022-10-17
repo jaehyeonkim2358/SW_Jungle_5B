2644c1fb7ddb384e6ac1929d3ed0bf2e6480f75b
rivolt0421 <dlrkddnr0421@daum.net>
2022-10-17 18:43:35 +0900

    이강욱 행렬 곱셈 순서 117404kb 576ms

---
+++ Week04/11049/11049_rivolt0421.py
@@ -0,0 +1,55 @@
+import sys
+sys.stdin = open("W03-W04/Week04/11049/input.txt","r")
+rl = sys.stdin.readline
+
+n = int(input())
+C = [[float('inf')] * (n) for _ in range(n)]
+d = []
+for i in range(n):
+    if i != 0:
+        d.append(int(rl().split()[1]))
+    else:
+        d += list(map(int, rl().split()))
+
+# C 초기화
+for i in range(n):
+    C[i][i] = 0
+    
+def go(n, C, d):
+    for L in range(1, n):
+        for i in range(0, n-L):     # i = 부분 문제 시작 행렬 인덱스
+            j = i + L               # j = 부분 문제 끝   행렬 인덱스
+
+            for k in range(i, j):
+                tmp = C[i][k] + C[k+1][j] + (d[i]*d[k+1]*d[j+1])
+                if C[i][j] > tmp:
+                    C[i][j] = tmp
+            
+    return C[0][n-1]
+
+print(go(n, C, d))
+
+# dp 테이블 미리 공간 만들어 놓지 않고, 하나씩 append하며 진행하는 방식.
+# 효율성에 차이가 없다.
+# n = int(input())
+# C = [[0] for _ in range(n)]
+# d = []
+# for i in range(n):
+#     if i != 0:
+#         d.append(int(rl().split()[1]))
+#     else:
+#         d += list(map(int, rl().split()))
+
+# def go(n, C, d):
+#     for L in range(1, n):
+#         for i in range(0, n-L):
+#             j = i + L
+#             min_ = float('inf')
+#             for k in range(i, j):
+#                 tmp = C[i][k-i] + C[k+1][j-(k+1)] + (d[i]*d[k+1]*d[j+1])
+#                 if min_ > tmp:
+#                     min_ = tmp
+#             C[i].append(min_)
+#     return C[0][n-1]
+
+# print(go(n, C, d))
