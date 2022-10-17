a5dc8a941243ac1e9430503f22dff3f6eb959bae
Jaehyeon <jaehyeonkim2358@gmail.com>
2022-10-17 16:12:31 +0900

    김재현 점프 47184kb 712ms

---
+++ Week04/2253/2253_jaehyeonkim2358.py
@@ -0,0 +1,24 @@
+from math import sqrt
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+
+trap = set(int(sys.stdin.readline().rstrip()) for _ in range(m))
+
+INF = float('inf')
+max_v = int(1.5*(n**.5)) + 2
+dp = [[INF] * (max_v) for _ in range(n+1)]
+dp[1][0] = 0
+
+for i in range(2, n+1):
+    if i in trap:
+        continue
+    for v in range(1, int(sqrt(2*i))+1):
+        dp[i][v] = min(dp[i-v][v-1], dp[i-v][v], dp[i-v][v+1]) + 1
+
+ans = min(dp[n])
+if ans == INF:
+    ans = -1
+sys.stdout.write(f'{ans}')
+
+
